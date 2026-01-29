f7578a9ebb8e4ca5e9ce60617525e75673765f97
oaao <oliver+github@abolish.golf>
2026-01-29 16:35:30 -0700

    rev [02 - reactive patterns]: ex01 - Step 2: make resource-results HTML template generic

--- 02 - reactive patterns/exercises/01 - component communication/instructor/src/js/components/resource-results.js
+++ 02 - reactive patterns/exercises/01 - component communication/instructor/src/js/components/resource-results.js
@@ -13,42 +13,9 @@ template.innerHTML = `
       </div>
 
       <div class="list-group list-group-flush">
-        <button type="button" class="list-group-item list-group-item-action active" aria-current="true">
-          <div class="d-flex w-100 justify-content-between">
-            <h2 class="h6 mb-1">Peer Tutoring Centre</h2>
-            <small>Academic</small>
-          </div>
-          <p class="mb-1 small text-body-secondary">Drop-in tutoring and study support.</p>
-          <small class="text-body-secondary">Building W, Room W101</small>
-        </button>
-
-        <button type="button" class="list-group-item list-group-item-action">
-          <div class="d-flex w-100 justify-content-between">
-            <h2 class="h6 mb-1">Counselling Services</h2>
-            <small>Wellness</small>
-          </div>
-          <p class="mb-1 small text-body-secondary">Confidential mental health supports.</p>
-          <small class="text-body-secondary">Virtual and in-person</small>
-        </button>
-
-        <button type="button" class="list-group-item list-group-item-action">
-          <div class="d-flex w-100 justify-content-between">
-            <h2 class="h6 mb-1">Student Awards and Bursaries</h2>
-            <small>Financial</small>
-          </div>
-          <p class="mb-1 small text-body-secondary">Funding options and application help.</p>
-          <small class="text-body-secondary">Student Services, Main Floor CAT</small>
-        </button>
-
-        <button type="button" class="list-group-item list-group-item-action">
-          <div class="d-flex w-100 justify-content-between">
-            <h2 class="h6 mb-1">IT Service Desk</h2>
-            <small>Tech</small>
-          </div>
-          <p class="mb-1 small text-body-secondary">Account access, Wi-Fi, BYOD support.</p>
-          <small class="text-body-secondary">Library</small>
-        </button>
+         <!-- results will be injected here, by selecting for .list-group and embedding inner HTML -->
       </div>
+
     </div>
   </section>`;
 
