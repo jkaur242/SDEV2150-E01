47cfa293948d8b8d22d0b17afddbd0ee1f7eb50c
oaao <oliver+github@abolish.golf>
2026-03-05 18:50:37 -0700

    rev [06 - state]: ex02 - (BREAKING) initial setup for Results processing filters

--- 06 - events + state/examples/02 - sharing state/instructor/src/components/Results.jsx
+++ 06 - events + state/examples/02 - sharing state/instructor/src/components/Results.jsx
@@ -14,27 +14,36 @@ export default function Results({
   return (
     <Card title="Results">
       <ul className="divide-y divide-gray-200">
-        {resources.map((r) => (
-          <ResultsItem
-            key={r.id}
-            title={r.title}
-            category={r.category}
-            summary={r.summary}
-            location={r.location}
-            onClick={() => onSelectResource(r)}
-            selected={selectedResource?.id === r.id}
-          >
-            {/* Above, I'm passing two new props to ResultsItem:
-                - onClick  -> what should happen when something is clicked (arrow function firing the setter)
-                - selected -> (for each result item), do a check to see if the item is selected or not
-            */}
-            {/* children: optional badge content */}
-            {r.openNow && (
-              <span className="rounded-full bg-emerald-100 px-2 py-0.5 text-[10px] font-semibold text-emerald-800">
-                Open now
-              </span>
-            )}
-          </ResultsItem>
+        {resources.filter((r) => {
+          // I want to check every element for whether it matches each filter option,
+          const matchesSearch
+          const matchesCategory
+          const matchesOpenNow
+          // Then return true or false as the result of this conditional expression if *all*
+          // those checks match (I might have multiple filters being used at once).
+          return matchesSearch && matchesCategory && matchesOpenNow
+        })
+          .map((r) => (
+            <ResultsItem
+              key={r.id}
+              title={r.title}
+              category={r.category}
+              summary={r.summary}
+              location={r.location}
+              onClick={() => onSelectResource(r)}
+              selected={selectedResource?.id === r.id}
+            >
+              {/* Above, I'm passing two new props to ResultsItem:
+                  - onClick  -> what should happen when something is clicked (arrow function firing the setter)
+                  - selected -> (for each result item), do a check to see if the item is selected or not
+              */}
+              {/* children: optional badge content */}
+              {r.openNow && (
+                <span className="rounded-full bg-emerald-100 px-2 py-0.5 text-[10px] font-semibold text-emerald-800">
+                  Open now
+                </span>
+              )}
+            </ResultsItem>
         ))}
       </ul>
     </Card >
